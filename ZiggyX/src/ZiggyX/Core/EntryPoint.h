9fd7dafc493ce844c4ec3ca9255faa3134e9406a
stevenfojas670 <84162024+stevenfojas670@users.noreply.github.com>
2026-07-11 12:42:12 -0700

    Removed readmens, keycodes, log, base.h, and windows.

--- ZiggyX/src/ZiggyX/Core/EntryPoint.h
+++ ZiggyX/src/ZiggyX/Core/EntryPoint.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #ifdef ZX_PLATFORM_WINDOWS
-	
+
 extern ZiggyX::Application* ZiggyX::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	// Initialize all subsystems here
+
 	auto app = ZiggyX::CreateApplication();
 	app->Run();
 	app->Stop();
--- ZiggyX/src/ZiggyX/Core/README.md
+++
@@ -1,4 +0,0 @@
-# Core
-The core is the application (or domain layer in relation to web dev) layer.
-The files in the core are all platform independent and should be implemented in
-their own files.
